b7a4a0cca713171fe93680438a9cafd4a14e0203
Gokulkrishna14 <nairgokul22@gmail.com>
2024-04-24 20:53:20 +0530

    Test cases failures

--- qtify/src/components/Logo/Logo.jsx
+++ qtify/src/components/Logo/Logo.jsx
@@ -1,5 +1,5 @@
 import LogoImage from "../../assets/logo.png";
 
 export default function Logo() {
-    return <img src={LogoImage} alt="Qtify" width={67} />
+    return <img src={LogoImage} alt="Qtify" width="auto" height={54} />
 }
